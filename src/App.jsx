e7be8d340a01ba9371dcc1a2cc7d1729b316c694
mohandoss21 <mohandossvelmurugan21@gmail.com>
2024-02-23 23:20:38 +0530

    Internal_Mark_Calculator

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import React from 'react'
+import Home from "./components/Home";
+function App() {
+  return (
+    <Fragment>
+      <Home/>
+    </Fragment>
+  );
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './style.css'
+import Form from './Form'
+function Home() {
+  return (
+    <div>
+      <nav>
+      <h1>Internal Mark Prediction</h1>
+      </nav>
+      <Form/>
+    </div>
+    
+  )
+}
+
+export default Home
